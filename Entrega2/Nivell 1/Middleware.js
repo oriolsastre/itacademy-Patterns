50145a5d183c68356becdb326a77a7fc8669b735
oriolsastre <oriol.sastre@gmail.com>
2022-11-28 10:01:25 +0100

    middleware fet

--- Entrega2/Nivell 1/Middleware.js
+++ Entrega2/Nivell 1/Middleware.js
@@ -4,7 +4,7 @@ class Middleware{
         this.dades = null;
 
         Object.getOwnPropertyNames(Object.getPrototypeOf(calculadora)).forEach(nomMetode => {
-            if(nomMetode != 'constructor'){
+            if(nomMetode !== 'constructor'){
                 this.digerirFuncio(calculadora[nomMetode])
             }
         })
@@ -16,18 +16,21 @@ class Middleware{
         this.middlewares.push(funcio);
     }
 
-    executarMW(index){
+    executarMW(index = 0){
         /* La funció digerida, en ser cridada, executa els MW i després fa el que fa normalment */
-        this.middlewares[index](this.dades,this.middlewares[index++])
+        /* El Middleware fa servir les dades emmagatzemades a la classe, i posa com a next el següent middleware a l'array */
+        /* L'últim middleware criadaria un index de l'array no definit, però l'ultim middleware no ha d'invocar la funcio next() */
+        this.middlewares[index].call(this, this.dades,() => this.executarMW(index+1))
     }
 
     digerirFuncio(funcio){
          /* Digerir mètodes de calculadora. Ara bé, primer executen els MW i després fan el que han de fer. */
         this[funcio.name] = (...args) => {
             this.dades = args[0];
-            this.executarMW(0);
+            this.executarMW();
 
-            return funcio(...args);
+            //return funcio(...args);   //valors sense canviar
+            return funcio(this.dades);  //valors canviats pels middlewares
         }
     }
 }
--- Entrega2/Nivell 1/index.js
+++ Entrega2/Nivell 1/index.js
@@ -10,24 +10,31 @@ const mwQuadrat = (data,next) => {
     const operand1 = Object.keys(data)[0];
     const operand2 = Object.keys(data)[1];
 
-    /*
+    console.log("El quadrat del primer operand és: "+data[operand1]**2)
+    console.log("El quadrat del segon operand és: "+data[operand2]**2)
+
     data[operand1] = data[operand1]*data[operand1];
     data[operand2] = data[operand2]*data[operand2];
-    */
-    console.log("El quadrat del primer operand és: "+data[operand1]*data[operand1])
-    console.log("El quadrat del segon operand és: "+data[operand2]*data[operand2])
-    //next();
+    
+    next();
 }
 const mwCub = (data,next) => {
     const operand1 = Object.keys(data)[0];
     const operand2 = Object.keys(data)[1];
-    console.log("El quadrat del primer operand és: "+data[operand1]**3)
-    console.log("El quadrat del segon operand és: "+data[operand2]**3)
-    //next();
+    console.log("El cub del primer operand és: "+data[operand1]**3)
+    console.log("El cub del segon operand és: "+data[operand2]**3)
+    next();
+}
+
+const mwDivisio = (data,next) => {
+    const operand1 = Object.keys(data)[0];
+    const operand2 = Object.keys(data)[1];
+    console.log("La divisió entre el 1er i 2n operand és: "+ data[operand1]/data[operand2])
 }
 
 novaCalculadora.afegirMW(mwQuadrat);
 novaCalculadora.afegirMW(mwCub);
+novaCalculadora.afegirMW(mwDivisio);
 
-console.log(novaCalculadora.multiplica(dades))
+console.log(novaCalculadora.suma(dades))
 
