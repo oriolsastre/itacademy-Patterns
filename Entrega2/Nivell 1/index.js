514548b507988f5a7d80e81bdff8810f45d89577
oriolsastre <oriol.sastre@gmail.com>
2022-11-28 13:20:21 +0100

    middleware

--- Entrega2/Nivell 1/index.js
+++ Entrega2/Nivell 1/index.js
@@ -13,28 +13,39 @@ const mwQuadrat = (data,next) => {
     console.log("El quadrat del primer operand és: "+data[operand1]**2)
     console.log("El quadrat del segon operand és: "+data[operand2]**2)
 
-    data[operand1] = data[operand1]*data[operand1];
-    data[operand2] = data[operand2]*data[operand2];
+    data[operand1] = data[operand1]**2;
+    data[operand2] = data[operand2]**2;
     
     next();
 }
 const mwCub = (data,next) => {
     const operand1 = Object.keys(data)[0];
     const operand2 = Object.keys(data)[1];
+    
     console.log("El cub del primer operand és: "+data[operand1]**3)
     console.log("El cub del segon operand és: "+data[operand2]**3)
+
+    data[operand1] = data[operand1]**3;
+    data[operand2] = data[operand2]**3;
     next();
 }
 
 const mwDivisio = (data,next) => {
     const operand1 = Object.keys(data)[0];
     const operand2 = Object.keys(data)[1];
-    console.log("La divisió entre el 1er i 2n operand és: "+ data[operand1]/data[operand2])
+    
+    console.log("La meitat del primer operand és: "+data[operand1]/2)
+    console.log("La meitat del segon operand és: "+data[operand2]/2)
+
+    data[operand1] = data[operand1]/2;
+    data[operand2] = data[operand2]/2;
 }
 
 novaCalculadora.afegirMW(mwQuadrat);
 novaCalculadora.afegirMW(mwCub);
 novaCalculadora.afegirMW(mwDivisio);
 
-console.log(novaCalculadora.suma(dades))
+console.log('La suma dels dos operands modificats és: '+novaCalculadora.suma(dades))
+console.log('La resta dels dos operands modificats és: '+novaCalculadora.resta(dades))
+console.log('La multiplicació dels dos operands modificats és: '+novaCalculadora.multiplica(dades))
 
